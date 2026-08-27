ad3b80a9e64c8853738ca25c864358ee5a7ac33d
Muhammad Fadhlan Aziz <muhammadfadhlan0011@gmail.com>
2026-08-27 14:20:07 +0700

    chore: comment out pacman eating gif sprite and update retro styles

--- src/components/retroSprites.tsx
+++ src/components/retroSprites.tsx
@@ -18,6 +18,7 @@ export function RetroSprites() {
           width: "56px",
           height: "56px",
           bottom: "12px",
+          zIndex: 20,
         }}
       >
         <Image
@@ -38,6 +39,7 @@ export function RetroSprites() {
           width: "68px",
           height: "68px",
           bottom: "12px",
+          zIndex: 20,
         }}
       >
         <Image
@@ -57,6 +59,7 @@ export function RetroSprites() {
           width: "40px",
           height: "40px",
           bottom: "8px",
+          zIndex: 20,
         }}
       >
         <Image
@@ -76,8 +79,17 @@ export function RetroSprites() {
           width: "48px",
           height: "48px",
           bottom: "48px",
+          zIndex: 20,
         }}
       >
+        {/* Sparkle glitter trail behind the bat */}
+        <div className="absolute inset-0 pointer-events-none" style={{ zIndex: -1 }}>
+          <div className="sparkle-pixel" style={{ width: "6px", height: "6px", top: "12px", right: "-10px", animationDelay: "0s" }} />
+          <div className="sparkle-pixel" style={{ width: "4px", height: "4px", top: "28px", right: "-20px", animationDelay: "0.2s" }} />
+          <div className="sparkle-pixel" style={{ width: "7px", height: "7px", top: "4px", right: "-15px", animationDelay: "0.4s" }} />
+          <div className="sparkle-pixel" style={{ width: "5px", height: "5px", top: "20px", right: "-28px", animationDelay: "0.1s" }} />
+          <div className="sparkle-pixel" style={{ width: "6px", height: "6px", top: "16px", right: "-36px", animationDelay: "0.3s" }} />
+        </div>
         <Image
           src="/images/gif/bat-flying.gif"
           alt="Bat"
@@ -96,6 +108,7 @@ export function RetroSprites() {
           height: "48px",
           bottom: "-1px",
           left: "4px",
+          zIndex: 10,
         }}
       >
         <Image
@@ -109,13 +122,14 @@ export function RetroSprites() {
         />
       </div>
 
-      {/* Mega Man Dance (Static shifted towards the center) */}
+      {/* Mega Man Dance (Moving from left to 20% width) */}
       <div
-        className="absolute left-[20%]"
+        className="absolute animate-megaman"
         style={{
           width: "40px",
           height: "40px",
           bottom: "0px",
+          zIndex: 10,
         }}
       >
         <Image
@@ -136,6 +150,7 @@ export function RetroSprites() {
           height: "48px",
           bottom: "0px",
           right: "0px",
+          zIndex: 10,
         }}
       >
         <Image
@@ -155,6 +170,7 @@ export function RetroSprites() {
           width: "32px",
           height: "32px",
           bottom: "-2px",
+          zIndex: 20,
         }}
       >
         <Image
@@ -167,6 +183,28 @@ export function RetroSprites() {
         />
       </div>
 
+      {/* Pacman (Moving in a screen-wide box loop)
+      <div
+        className="absolute animate-pacman"
+        style={{
+          width: "36px",
+          height: "36px",
+          bottom: "12px",
+          left: "0px",
+          zIndex: 20,
+        }}
+      >
+        <Image
+          src="/images/gif/pacman-eating.gif"
+          alt="Pacman"
+          width={36}
+          height={36}
+          className="pixelated object-contain"
+          unoptimized
+          priority
+        />
+      </div> */}
+
       {/* Bocchi Stroke (Static on the ground in the middle-left area) */}
       {/* <div
         className="absolute left-[35%]"
@@ -175,6 +213,7 @@ export function RetroSprites() {
           height: "36px",
           bottom: "20px",
           right: "15%",
+          zIndex: 10,
         }}
       >
         <Image
@@ -194,6 +233,7 @@ export function RetroSprites() {
           width: "48px",
           height: "48px",
           bottom: "6px",
+          zIndex: 20,
         }}
       >
         <Image
